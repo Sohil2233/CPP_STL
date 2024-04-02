1b370b92c5419ba84d512c73071d850de213964c
Sohil2233 <tambolisohil04@gmail.com>
2024-04-02 20:56:19 +0530

    Additional info

---
+++ 11Algorithm2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 5, 7, 7, 8, 10, 10, 10, 11, 11, 12};
+    int n = 11;
+    int x = 10;
+    //  First occurence
+    // 1st way
+    // if (binary_search(arr, arr + n, x))
+    // {
+    //     cout << lower_bound(arr, arr + n, x) - arr << endl;
+    // }
+    // else
+    //     cout << "does not exists";
+
+    // // 2nd way
+    // int ind = lower_bound(arr, arr + n, x) - arr;
+    // if (ind != n && arr[ind] == x)
+    // {
+    //     cout << "zFound at " << ind << endl;
+    // }
+    // else
+    //     cout << "Not Found " << endl;
+
+
+
+}
